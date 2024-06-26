5ba63153b6a72bf35f0915807fd8053ccbba9792
khurshid <Khurshid@qurilo.com>
2024-06-26 16:49:38 +0530

    login functionality created successfully and the jwt token created, update in .env logincontroller and authroute

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const UserModel = require("../models/userModel");
 
@@ -7,22 +7,24 @@ exports.login = async (req, res) => {
 
   try {
     const user = await UserModel.findOne({ username });
-    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Password did not match" });
+    }
 
-    const payload = { user: { id: user.id } };
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "30d",
+    });
 
-    jwt.sign(
-      payload,
-      "your_jwt_secret",
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
+    // Set token as a cookie
+    res.cookie("token", token, { httpOnly: true });
+
+    // Send response with status 200 and JSON object
+    return res.status(200).json({ message: "Admin logged in successfully", token });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
 const {register} = require("../controllers/signupController")
+const {login} = require("../controllers/loginController")
 
 const router = express.Router();
 
 
-// Route for user login 
+// Route for admin register
 router.post("/register-admin",register);
+
+// Router for admin Login
+
+router.post("/login",login);
 
 
 module.exports = router;
