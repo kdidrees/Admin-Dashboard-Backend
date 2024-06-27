a00172063cf63c975b2f785526e8d8424b4bc4db
khurshid <Khurshid@qurilo.com>
2024-06-27 19:04:47 +0530

    update in property route

--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -2,10 +2,14 @@ const express = require("express");
 const { createProperty } = require("../controllers/createProperty");
 const upload = require("../middlewares/upload");
 const verifyToken = require("../middlewares/jwtMiddleware");
+const {editProperty} = require("../controllers/editProperty")
 
 const router = express.Router();
 
 // Route for creating new listing
 router.post("/create-property", verifyToken, upload, createProperty);
+
+// Route to edit property 
+router.post("/edit-property/:id",editProperty);
 
 module.exports = router;
