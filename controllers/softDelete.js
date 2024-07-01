23d799ed62f4d4b0e993e513dbf30393f5a88414
khurshid <Khurshid@qurilo.com>
2024-07-01 15:41:28 +0530

    update in .env , softdelete ,,propertyroute  files

--- controllers/softDelete.js
+++ controllers/softDelete.js
@@ -1,1 +1,25 @@
-const PropertyModel = require('../models')
+const PropertyModel = require("../models/propertyModel");
+
+exports.deletePropertyById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const property = await PropertyModel.findById(id);
+
+    if (!property) {
+      return res.status(404).json({ message: "property not found" });
+    }
+
+    // perform soft delete by updating the property
+
+    property.deleted = true;
+    await property.save();
+
+    res
+      .status(200)
+      .json({ id: property._id, message: "property deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "internal server error" });
+  }
+};
--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -6,6 +6,7 @@ const { editProperty } = require("../controllers/editProperty");
 const { allProperty } = require("../controllers/allProperty");
 const { getPropertyById } = require("../controllers/getPropertyById");
 const {FilterProperties} = require("../controllers/filterController");
+const {deletePropertyById} = require("../controllers/softDelete");
 
 const router = express.Router();
 
@@ -24,5 +25,8 @@ router.get("/getpropertybyid/:id",getPropertyById);
 
 // route for filtering properties
 router.get('/filter',FilterProperties);
+
+// soft delete the property by id 
+router.delete('/delete-property/:id',deletePropertyById);
 
 module.exports = router;
